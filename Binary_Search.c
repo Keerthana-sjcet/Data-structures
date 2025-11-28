d0abb1d1d7abac1ba3b3243f2c5686375856dd10
Keerthana G Pillai <keerthanagpillai2028@cs.sjcetpalai.ac.in>
2025-11-29 03:15:09 +0530

    Add binary search implementation in Binary_Search.c
    Implement binary search algorithm to find an element in a sorted array.

---
+++ Binary_Search.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main() {
+    int n, target;
+    printf("Enter size of sorted array: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter %d sorted elements:\n", n);
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+    printf("Enter element to search: ");
+    scanf("%d", &target);
+    int low = 0, high = n - 1;
+    int foundIndex = -1;
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+        if (arr[mid] == target) {
+            foundIndex = mid;
+            break;
+        } 
+        else if (arr[mid] < target) {
+            low = mid + 1;
+        }
+        else {
+            high = mid - 1;
+        }
+    }
+    if (foundIndex == -1)   printf("Element not found.\n");
+    else printf("Element found at index %d\n", foundIndex);
+    printf("Time complexity:O(log n)\n Space complexity :O(1)") ;
+    return 0;
+}
